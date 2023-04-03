c4485bb42062768e8547fbc6fc071842c491c9c4
mario.tarantini <mario.tarantini@deesup.com>
2023-04-03 21:15:53 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # orangelooptest
+
+Questa è la mia soluzione all'evaluation test.
+
+Per ricevere le risposte al test clonare il repository e nel terminale eseguir i seguenti comandi (all'interno della cartella del repo) : 
+
+- npm install
+- npm start
+- le soluzioni saranno mostrate in console
+
+Per quanto riguarda la seconda domanda, la mia funzione ha come parametri le lunghezze degli array e genera gli array a seconda della lunghezza passata.
+
--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const app = express();
 app.listen(3000, () => {
-    //Use getResult.toString() to return result without brackets
-    console.log(JSON.parse({
+    console.log({
         firstAnswer: getFirstAnswerResult(),
-        secondAnswer: "",
-        thirdAnswer: "",
+        secondAnswer: arrayAlgorithm(10, 9),
+        thirdAnswer: "Inizio dal task con il quale mi sento più confidente per chiuderlo nel minor tempo possibile, in questo modo avrò maggior tempo per svolgere il task dove mi sento meno sicuro",
         fourthAnswer: "",
-    }));
+    });
 });
-const getNumbers = () => {
-    return ([...Array(100).keys()].map(x => ++x));
-}
 const getFirstAnswerResult = () => {
     return getNumbers().map(n => {
         return multipleCheck(n);
     });
+}
+const getNumbers = () => {
+    return ([...Array(100).keys()].map(x => ++x));
 }
 const multipleCheck = (n) => {
     const isThreeMultiple = (n % 3) === 0 ? true : false;
@@ -31,7 +30,22 @@ const multipleCheck = (n) => {
         return n;
     }
 }
-
-const workingArray = (n, k) => {
-
+const arrayAlgorithm = (n, k) => {
+    const firstArray = [...Array(n).keys()].map(el => {
+        return Math.random();
+    });
+    const secondArray = [...Array(k).keys()].map(el => {
+        return Math.random();
+    });
+    const result = secondArray.map(el => {
+       return {
+        key: el,
+        value: firstArray.filter(sel => sel < el).length
+        };
+    });
+    return {
+        firstArray,
+        secondArray,
+        result: JSON.stringify(result, null, 4)
+    }
 }
